e92fbc6c7c3cd59862fa674a24caa88f4200d1cf
Augusto Gonçalves Santos <augustogsantos@outlook.com.br>
2025-07-09 03:25:06 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 goncalvesAugusto7/clone-tabnews
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Feliz aniversário!</h1>
+function Home() {
+  return <h1>Feliz aniversário!</h1>;
 }
 
 export default Home;
+
+function Teste() {
+  return <h1>nada</h1>;
+}
